f74f5d694fc0e81d4564bfc96b771dc968da187a
DESKTOP-OAUP1PE\User <j58701076@yahoo.com.tw>
2022-02-15 22:26:16 +0800

    123

--- python-flask/index.py
+++ python-flask/index.py
@@ -7,6 +7,7 @@ from flask import redirect # 載入redirect函式
 from flask import render_template # 使用樣板引擎
 from flask import session # 使用session
 
+
 #嘗試連接到資料庫
 mydb = mysql.connector.connect(
     host="localhost",
@@ -15,7 +16,10 @@ mydb = mysql.connector.connect(
     database="mydatabase"
 )
 
+mycursor=mydb.cursor()
+
 print("資料庫訊息:",mydb)
+mydb.close() #關閉資料庫
 
 # 建立 Application物件，可以設定靜態檔案的路徑處理
 # 所有在 static 資料夾底下的檔案，都對應到網址路徑 /static/ 檔案名稱
@@ -43,8 +47,8 @@ def signin():
     password = request.form['psw']
     if username=="" or password=="":
         return redirect('/empty/?message=帳號或密碼不能為空')
-    got=mydb.cursor().execute("SELECT username FROM member WHERE username=%s",(username,))
-    result=mydb.cursor().fetchone()
+    got=mycursor.execute("SELECT username FROM member WHERE username=%s",(username,))
+    result=mycursor.fetchone() #我只需要讀取一個紀錄
     mydb.commit()
     if got == 0:
         return redirect("error/?message=帳號或密碼錯誤")
@@ -56,7 +60,7 @@ def signin():
             return redirect("error/?message=帳號或密碼錯誤")
     else:
         return redirect("error/?message=全部都錯")
-    
+mydb.close() #關閉資料庫 
 
 #建立路徑 /signup對應的處理函式，獲取註冊請求及處理
 @app.route("/signup",methods=['POST'])
@@ -64,19 +68,19 @@ def signup():
     name = request.form['nickname'] #取得表格輸入的姓名
     username = request.form['new_ac'] #取得表格輸入的新帳號
     password = request.form['new_psw'] #取得表格輸入的新密碼
-    cur = mydb.cursor()
-    got=cur.execute("SELECT username FROM member where username=%s",(username,))
+    mycursor = mydb.cursor()
+    got=mycursor.execute("SELECT username FROM member where username=%s",(username,))
     mydb.commit()
-    # result = cur.fetchall()
+    # result = mycursor.fetchall()
     if got!=0:
        return redirect("/error/?message=註冊失敗，該用戶已被註冊")    
     else:
-            result = cur.execute("INSERT INTO member (name,username,password) VALUES (%s,%s,%s)",
+            result = mycursor.execute("INSERT INTO member (name,username,password) VALUES (%s,%s,%s)",
                 (name,username,password))
             mydb.commit()
             print("新增",result,"筆，恭喜記錄成功")
             return redirect("/")
-   
+mydb.close() #關閉資料庫 
 
 # 建立路徑 /member對應的處理函式
 @app.route("/member/",methods=['GET'])
